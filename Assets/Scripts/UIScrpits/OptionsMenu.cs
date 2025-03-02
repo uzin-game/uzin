34aa4518ee269323ea337e62472c0faa98b08ef8
Tristan Druart <tristan.druart@epita.fr>
2025-03-02 10:12:50 +0100

    one ssaie de phs

---
+++ Assets/Scripts/UIScrpits/OptionsMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+    
+    private Resolution[] resolutions;
+    
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        
+        resolutionDropdown.ClearOptions();
+        
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void setVolume(float volume)
+    {
+        mixer.SetFloat("Volume", volume);
+    }
+
+    public void SetQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Screen.SetResolution(resolutions[resolutionIndex].width, resolutions[resolutionIndex].height, Screen.fullScreen);
+    }
+}
