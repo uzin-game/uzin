85e02e902fd9f946ed2c4f2f2eeb6551cb82a2fa
Tristan Druart <tristan.druart@epita.fr>
2025-05-20 20:31:41 +0200

    started quest feature

---
+++ Assets/Scripts/QuestsScrpit/Missions/ExploreMap.cs
@@ -0,0 +1,12 @@
+﻿namespace QuestsScrpit.Missions
+{
+    public class ExploreMap : Quest.QuestGoal
+    {
+        public override string GetDescription()
+        {
+            return $"Explorez les environnements";
+        }
+        
+        private void OnExplore(){}
+    }
+}
---
+++ Assets/Scripts/QuestsScrpit/Quest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using QuestsScrpit;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace QuestsScrpit
+{
+    public class Quest : ScriptableObject
+    {
+        [System.Serializable]
+        public struct Info
+        {
+            public string Name;
+            public string Description;
+        }
+        
+        [Header("Info")] public Info Information;
+
+        public bool Completed { get; protected set; }
+        public QuestCompletedEvent QuestCompleted;
+        public abstract class QuestGoal : ScriptableObject
+        {
+            protected string Description;
+            public int CurrentAmount { get; protected set; }
+            public int RequiredAmount;
+            
+            public bool Completed { get; protected set; }
+            [HideInInspector] public UnityEvent OnCompleted;
+
+            public virtual string GetDescription()
+            {
+                return Description;
+            }
+
+            public virtual void Initialize()
+            {
+                Completed = false;
+            }
+
+            protected void Evaluate()
+            {
+                if (CurrentAmount >= RequiredAmount)
+                {
+                    Complete();
+                }
+            }
+
+            private void Complete()
+            {
+                Completed = true;
+                OnCompleted?.Invoke();
+                OnCompleted?.RemoveAllListeners();
+            }
+        }
+        
+        public List<QuestGoal> Goals;
+
+        public void Initialize()
+        {
+            Completed = false;
+            QuestCompleted = new QuestCompletedEvent();
+
+            foreach (var goal in Goals)
+            {
+                goal.Initialize();
+                goal.OnCompleted.AddListener(delegate { CheckGoals(); });
+            }
+        }
+
+        private void CheckGoals()
+        {
+            Completed = Goals.All(g => g.Completed);
+            if (Completed)
+            {
+                QuestCompleted.Invoke(this);
+                QuestCompleted.RemoveAllListeners();
+            }
+        }
+
+    }
+}
+
+public class QuestCompletedEvent : UnityEvent<Quest>
+{
+    
+}
